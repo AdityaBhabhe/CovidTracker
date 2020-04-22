2f85f7fa92ab03328b21a043c79582ba59d3aa7a
Aditya Bhabhe <aditya.bhabhe97@gmail.com>
2020-04-22 22:15:18 +0530

    Updates

---
+++ precache-manifest.b9178f8deb6a4ba364a1476ef0e6979f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5cc78c17c4d12a960aa402267dec48",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "9fcb07f82ffa83dd3f69",
+    "url": "/CovidTracker/static/css/main.615045a7.chunk.css"
+  },
+  {
+    "revision": "8fe0171fa1687eecc82a",
+    "url": "/CovidTracker/static/js/2.045c0cde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CovidTracker/static/js/2.045c0cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fcb07f82ffa83dd3f69",
+    "url": "/CovidTracker/static/js/main.652dd111.chunk.js"
+  },
+  {
+    "revision": "f24a48d69bf495da1ca8",
+    "url": "/CovidTracker/static/js/runtime-main.88d0797f.js"
+  }
+]);
